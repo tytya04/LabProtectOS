7368b10999022583bccf9ebace91db4a7e47d93d
tytya04 <tytya04@mail.ru>
2024-11-01 13:52:59 +0300

    готово вроде

--- lab5/main.c
+++ lab5/main.c
@@ -77,7 +77,6 @@ int addFile(char *archname, char *filename) {
         return 1;
     }
 
-    // Открываем архив для записи и добавляем новый файл
     archive = open(archname, O_WRONLY | O_APPEND);
     if (archive == -1) return 1;
 
@@ -87,14 +86,13 @@ int addFile(char *archname, char *filename) {
     time_t now;
     time(&now);
 
-    // Заполняем структуру для нового файла
     fileInfo newFileInfo;
     strncpy(newFileInfo.fileName, filename, sizeof(newFileInfo.fileName) - 1);
     newFileInfo.fileName[sizeof(newFileInfo.fileName) - 1] = '\0';
     newFileInfo.fileStat = file_info;
     newFileInfo.time = now;
 
-    // Записываем информацию о файле и содержимое файла в архив
+    // Записываем информацию о файле в архив
     if (write(archive, &newFileInfo, sizeof(fileInfo)) != sizeof(fileInfo)) {
         printf("Ошибка записи информации о файле %s в архив\n", filename);
         close(archive);
@@ -167,58 +165,72 @@ int deleteFile(char *archname, char *filename) {
 
 // Извлечение
 int extractFile(char *archname, char *filename) {
-	createArchive(".bufArch");
+    int archive = open(archname, O_RDONLY);
+    if (archive == -1) return 1;
 
-	int bufArch = open(".bufArch", O_WRONLY);
-	int archive = open(archname, O_RDONLY);
-	if (archive == -1) return 1;
+    fileInfo a;
+    bool fileExist = false;
 
-	bool fileExist = 1;
+    // Поиск файла в архиве
+    while (read(archive, &a, sizeof(fileInfo)) == sizeof(fileInfo)) {
+        if (strcmp(a.fileName, filename) == 0) {
+            fileExist = true;
+            break;
+        }
+        lseek(archive, a.fileStat.st_size, SEEK_CUR); // Пропуск содержимого файла
+    }
 
-	while (1) {
-		char *buf = malloc(sizeof(fileInfo));
-		if (!read(archive, buf, sizeof(fileInfo))) break;
-		fileInfo *a = (fileInfo*)(buf);
+    if (!fileExist) {
+        printf("Файл %s не найден в архиве\n", filename);
+        close(archive);
+        return 1;
+    }
 
-		lseek(archive, sizeof(fileInfo), SEEK_CUR);
-		char bufFile[a->fileStat.st_size];
-		read(archive, bufFile, a->fileStat.st_size);
+    // Проверка на перезапись, если файл существует
+    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+    char flag;
+    if (access(filename, F_OK) != -1) {
+        printf("Файл %s будет перезаписан. Вы хотите продолжить [y/n]?\n", filename);
+        scanf(" %c", &flag);
+        if (flag == 'n') {
+            close(archive);
+            return 2;
+        }
+    }
 
-		// Если текущий файл не тот, что извлекаем, записываем его в временный архив
-		if (strcmp(a->fileName, filename)) {
-			write(bufArch, a, sizeof(fileInfo));
-			lseek(bufArch, sizeof(fileInfo), SEEK_CUR);
-			write(bufArch, &bufFile, a->fileStat.st_size);
-		} else {
-			mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+    // Создание нового файла для извлечения
+    int file = creat(filename, mode);
+    if (file == -1) {
+        printf("Ошибка создания файла %s\n", filename);
+        close(archive);
+        return 1;
+    }
 
-			// Предупреждение о перезаписи, если файл существует
-			char flag;
-			if (access(filename, F_OK) != -1) {
-			    printf("Файл %s будет перезаписан. Вы хотите продолжить [y/n]?\n", filename);
-			    scanf("%c", &flag);
-			    if (flag == 'n') return 2;
-			}
-    
-			// Создание и запись файла
-			int file = creat(filename, mode);
-			write(file, &bufFile, a->fileStat.st_size);
-			close(file);
-			fileExist = 0;
+    // Чтение содержимого из архива и запись его в файл
+    char *bufFile = malloc(a.fileStat.st_size);
+    if (read(archive, bufFile, a.fileStat.st_size) != a.fileStat.st_size) {
+        printf("Ошибка извлечения содержимого файла %s\n", filename);
+        free(bufFile);
+        close(archive);
+        close(file);
+        return 1;
+    }
 
-			chmod(filename, a->fileStat.st_mode); // Установка прав доступа на файл
-		}
+    if (write(file, bufFile, a.fileStat.st_size) != a.fileStat.st_size) {
+        printf("Ошибка записи содержимого файла %s на диск\n", filename);
+        free(bufFile);
+        close(archive);
+        close(file);
+        return 1;
+    }
 
-		free(buf);
-	}
+    free(bufFile);
+    close(file);
+    close(archive);
 
-	close(archive);
-	close(bufArch);
-
-	remove(archname);
-	rename(".bufArch", archname);
-
-	return fileExist;
+    // Установка прав доступа на извлечённый файл
+    chmod(filename, a.fileStat.st_mode);
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
