ab3212252dcc5aee9951bbe518336095e465d347
tytya04 <tytya04@mail.ru>
2024-11-08 15:35:01 +0300

    ttt

--- lab4/main.c
+++ lab4/main.c
@@ -37,7 +37,6 @@ void apply_symbolic_mode(const char *mode_str, mode_t *current_mode) {
             char op = mode_str[i];
             i++;
 
-
             // Разбор прав доступа
             while (mode_str[i] && (mode_str[i] == 'r' || mode_str[i] == 'w' || mode_str[i] == 'x')) {
                 switch (mode_str[i]) {
@@ -111,6 +110,17 @@ void print_permissions(mode_t mode) {
     printf("\n");
 }
 
+void mode_to_string(mode_t mode, char *buffer) {
+    for (int i = 0; i < 9; i++) {
+        if (mode & (1 << (8 - i))) {
+            buffer[i] = (i % 3 == 0) ? 'r' : (i % 3 == 1) ? 'w' : 'x';
+        } else {
+            buffer[i] = '-';
+        }
+    }
+    buffer[9] = '\0';
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <mode> <file>\n", argv[0]);
@@ -123,24 +133,30 @@ int main(int argc, char *argv[]) {
     struct stat file_stat;
 
     if (stat(file_name, &file_stat) < 0) {
-
         perror("stat");
         return EXIT_FAILURE;
     }
 
-    mode_t mode = file_stat.st_mode;
+    mode_t old_mode = file_stat.st_mode;
+    mode_t new_mode = old_mode;
 
     if (mode_str[0] >= '0' && mode_str[0] <= '9') {
-        mode = parse_numeric_mode(mode_str);
+        new_mode = parse_numeric_mode(mode_str);
     } else {
-        apply_symbolic_mode(mode_str, &mode);
+        apply_symbolic_mode(mode_str, &new_mode);
     }
 
-    if (chmod(file_name, mode) < 0) {
+    if (chmod(file_name, new_mode) < 0) {
         perror("chmod");
         return EXIT_FAILURE;
     }
 
-    printf("Permissions for file %s changed successfully.\n", file_name);
+    // Преобразуем старые и новые права в строковый формат
+    char oldPerms[10], newPerms[10];
+    mode_to_string(old_mode, oldPerms);
+    mode_to_string(new_mode, newPerms);
+
+    printf("Права доступа для '%s' изменены с %s на %s\n", file_name, oldPerms, newPerms);
+
     return EXIT_SUCCESS;
 }
